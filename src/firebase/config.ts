6b0bd1e377d0f43da688e665accfc9586e677324
Albert John <aljohme@gmail.com>
2026-02-20 23:43:48 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/con

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,5 +4,6 @@ export const firebaseConfig = {
   appId: "1:757855378687:web:a91971e217a3c8085c80ca",
   apiKey: "AIzaSyCRFe1ASTr_Hej4cKu-3gD-VTJ-lZt3ZRQ",
   authDomain: "studio-1862145107-b405d.firebaseapp.com",
+  measurementId: "",
   messagingSenderId: "757855378687",
 };
